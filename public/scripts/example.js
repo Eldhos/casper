78eba9f1fa08e3112887189a8091ebc176f92cec
ldo <eldhosemannekkattu@gmail.com>
2016-08-05 00:40:17 +0530

    Partial Form

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,12 +1,31 @@
 var SearchForm = React.createClass({
+  getInitialState: function(){
+    return ({origin:'',destination:''});
+  },
+  handleOriginChange: function(e){
+    this.setState({origin:e.target.value});
+  },
+  handleDestinationChange: function(e) {
+    this.setState({destination:e.target.value});
+  },
+
   render: function(){
     return(
-      <div> {this.props.url}</div>
+      <form className = "searchForm" onSubmit={this.handleSubmit}>
+      <input
+        type="text"
+        placeholder="Origin"
+        value={this.state.origin}
+        onChange={this.handleOriginChange}
+        />
+        </form>
+
     );
   }
 });
 
+
 ReactDOM.render(
   <SearchForm url="/search"/>,
-  document.getElementById("content");
+  document.getElementById("content")
 );
