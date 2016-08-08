4b462017d84f60e36e73040544b57a8b6679c174
ldo <eldhosemannekkattu@gmail.com>
2016-08-08 22:47:14 +0530

    SearchFrom 80% Hurraygit add public/scripts/example.js git add public/scripts/example.js !

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,15 +1,48 @@
 var SearchForm = React.createClass({
-  addOriginDestination: function(){
+  getInitialState: function(){
+    return {
+      origin:'',
+      destination:'',
+    }
+
+  },
+  addOriginDestination: function(e){
+    e.preventDefault();
+    var origin = this.state.origin;
+    var destination = this.state.destination;
+    if(!origin || !destination){
+      return;
+    }
+    this.props.addToOriginDestination({origin: origin, destination:destination});
+    this.setState({origin:'', destination:''});
+  },
+  handleSubmit: function(e){
+    e.preventDefault();
+    var origin = this.state.origin;
+    var destination = this.state.destination;
+    if(!origin || !destination){
+      this.props.search(null);
+    } else {
+      this.props.search({origin:origin, destination: destination});
+
+    }
+    this.setState({origin:'',destination:''});
+  },
+  handleOriginChange: function(e){
+    this.setState({origin:e.target.value});
+  },
+  handleDestinationChange: function(e){
+    this.setState({destination: e.target.value});
   },
   render: function(){
     return (
       <div>
-      <form className = "form-inline" onSubmit={this.props.handleSubmit}>
+      <form className = "form-inline" onSubmit={this.handleSubmit}>
       <div className="form-group">
         <input className="form-control" type="text" placeholder="Origin"
-          value={this.props.origin} />
+          value={this.state.origin} onChange={this.handleOriginChange}/>
         <input className="form-control" type="text" placeholder="Destination"
-          value={this.props.destination} />
+          value={this.state.destination} onChange={this.handleDestinationChange}/>
         <button className="btn glyphicon glyphicon-search" type="submit"></button>
         <button className="btn glyphicon glyphicon-plus" onClick={this.addOriginDestination}></button>
       </div>
@@ -35,11 +68,12 @@ var SearchList = React.createClass({
 var SearchNode = React.createClass({
   render: function(){
     return(
-      <div>
-        <p>Origin: {this.props.origin}   Destination: {this.props.destination}</p>
-        <button className="btn btn-danger glyphicon glyphicon-trash"></button>
-      </div>
-
+      <form className="form-inline">
+        <div className="form-group">
+          <input className="form-control" type="text" value={this.props.origin} disabled/>
+          <input className="form-control" type="text" value={this.props.destination} disabled/>
+        </div>
+      </form>
     );
   }
 });
@@ -52,11 +86,33 @@ var SearchContainer = React.createClass({
       newForm:false
     }]};
   },
+  addToOriginDestination:function(search){
+    var currentSearch = this.state.data;
+    search.id = Date.now();
+    this.setState({data: currentSearch.concat([search])});
+  },
+  doTheThing: function(search){
+    var searchData=this.state.data;
+    var sendSearch = searchData;
+    if(search != null) {
+      search.id = Date.now();
+      sendSearch = searchData.concat([search])
+    }
+    fetch('/search', {
+           method: 'POST',
+        headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+                },
+          body: JSON.stringify(sendSearch)
+     });
+     this.setState({data:sendSearch});
+  },
   render: function(){
     return (
       <div className="searchContainer">
         <SearchList results={this.state.data} url={this.props.url}/>
-        <SearchForm/>
+        <SearchForm search={this.doTheThing} addToOriginDestination={this.addToOriginDestination}/>
       </div>
             );
   }
